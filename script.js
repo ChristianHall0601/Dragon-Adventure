c299f6ed70d33e64cd0b8ab4213b2b0855b02e6c
Christian Hall <christianhall0601@gmail.com>
2024-04-26 13:49:42 -0500

    fixed autoplay

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 menuSong = document.getElementById("menu_audio");
 hoverSelection = document.getElementById("hover_selection");
-menuSong.play();
 
 function playHover(){
     hoverSelection.currentTime = 0;
     hoverSelection.play();
+}
+
+window.onload = playMain();
+
+function playMain(){
+    menuSong.play();
 }
