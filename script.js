7e9bb7c31368b266392aedd3f48b8193bf1329bd
Christian Hall <christianhall0601@gmail.com>
2024-04-30 08:48:22 -0500

    fixed the starting buttons

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 //Sounds
 menuSong = document.getElementById("menu_audio");
 hoverSelection = document.getElementById("hover_selection");
-clickSelection = document.getElementById("click_selection")
+clickSelection = document.getElementById("click_selection");
+fakeMenuBtns = document.getElementsByClassName("fakeButton");
 
 //Functions
 function playHover(){
@@ -14,10 +15,15 @@ function playSelected(){
 }
 
 function playMain(){
-    menuSong.volume = 0.5
+    menuSong.volume = 0.5;
     menuSong.play();
+    setTimeout(stopAnimating, 1200)
 }
 
+function stopAnimating(){
+    for (let i = 0; i< fakeMenuBtns.length; i++){
+        (fakeMenuBtns[i]).style.display = "block"
+    }
+}
 
 window.onload = playMain();
-
