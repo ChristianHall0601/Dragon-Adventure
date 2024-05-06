5f65edbb6c59629f897b42b880144dbfaa0d6c90
Christian Hall <chall2541@tuskegee.edu>
2024-05-06 12:19:11 -0500

    added help screen

--- script.js
+++ script.js
@@ -2,9 +2,17 @@
 menuSong = document.getElementById("menu_audio");
 hoverSelection = document.getElementById("hover_selection");
 clickSelection = document.getElementById("click_selection");
+
+//btns
 fakeBtns = document.getElementsByClassName("fakeButton");
 playBtn = document.getElementById("playBtn");
-helpBtn = document.getElementById("helpBtn")
+helpBtn = document.getElementById("helpBtn");
+backBtn = document.getElementById("backBtn");
+
+//screens
+startScreen = document.getElementById("startscreen");
+helpScreen = document.getElementById("helpscreen");
+titleName = document.getElementById("titleName");
 
 //Functions
 function playHover(){
@@ -29,6 +37,23 @@ function showMyStartBtns(){
     }
 }
 
+function switchToHelpScreen(){
+    titleName.style.animation = "buttonOnOpen 0.5s reverse forwards";
+    playBtn.style.animation = "buttonOnOpen 0.5s reverse forwards";
+    setTimeout(helpBtnDissappear, 400);
+    setTimeout(helperSwticherTH, 2000);
+    
+}
+
+function helpBtnDissappear(){
+    helpBtn.style.animation = "buttonOnOpen 0.5 reverse forwards"
+}
+
+function helperSwticherTH(){
+    startScreen.style.display = "none"
+    helpScreen.style.display = "block";
+}
+
 function highlightPlayBtn(){
     playBtn.style.animation = "coolAnimation 0.2s both"
 }
@@ -45,10 +70,19 @@ function deactivateHelpBtn(){
     helpBtn.style.animation = "coolAnimation 0.2s backwards"
 }
 
+function switchToMainScreen(){
+    helpScreen.style.display = "none";
+    startScreen.style.display = "block";
+    titleName.style.animation = "buttonOnOpen 0.2s forwards";
+    playBtn.style.animation = "buttonOnOpen 0.2s forwards";
+}
+
 playBtn.addEventListener("mouseover", highlightPlayBtn)
 playBtn.addEventListener("mouseout", deactivatePlayBtn)
 helpBtn.addEventListener("mouseover", highlightHelpBtn)
 helpBtn.addEventListener("mouseout", deactivateHelpBtn)
+helpBtn.addEventListener("click", switchToHelpScreen)
+backBtn.addEventListener("click", switchToMainScreen)
 
 window.onload = playMain();
 
